e7f9eb53b574349e32477064ddfd5d39bf4eae5f
PavanTeja5 <padalapavanteja@gmail.com>
2023-02-27 17:06:55 +0530

    webpage done

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, jsonify
+import os
+import numpy as np
+import joblib
+import yaml
+
+params_path = "params.yaml"
+webapp_root = "webapp"
+
+static_dir = os.path.join(webapp_root, "static")
+template_dir = os.path.join(webapp_root, "templates")
+
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(data)
+    print(prediction)
+    return prediction[0]
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json.values())])
+        response = predict(data)
+        response = {"response":response}
+        return response
+    except Exception as e:
+        print(e)
+        error = {"error": "Something went wrong!! Try again"}
+        return error
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        try:
+            if request.form:
+                data = dict(request.form).values()
+                print(data)
+                # data = [list(map(float, data))]
+                data = [[float(n[0]) for n in list(data)]]
+                print(data)
+                response = predict(data)
+                print(response)
+                return render_template("index.html", response=response)
+
+            elif request.json:
+                response = api_response(request)
+                return jsonify(response)
+
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went wrong!! Try again"}
+            return render_template("404.html", error=error)
+        pass
+    else:
+        return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
